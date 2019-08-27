44cce596091ba5cf36159f7dfe7420a105c1c593
Yuichiro Dan <yuichirodan1022@gmail.com>
2019-08-28 07:10:07 +0900

    Fix flow/EdmonsKarp.cpp

--- flow/EdmonsKarp.cpp
+++ flow/EdmonsKarp.cpp
@@ -12,12 +12,13 @@
 using T = int;
 
 using Edge = std::map<std::string, T>; //for this algorithm, an edge must have "pred", "suc", "capasity" and "flow" keys.
-using FlowNetwork = std::vector<std::pair<std::vector<Edge>, std::vector<Edge> > >; //the index of the vector means the id of the predecessor. the elements of the pair of vectors of edges mean "forward" and "backward."
+using FlowNetwork = std::vector<std::pair<std::vector<std::reference_wrapper<Edge> >, std::vector<std::reference_wrapper<Edge> > > >; //the index of the vector means the id of the predecessor. the elements of the pair of vectors of edges mean "forward" and "backward."
 using PathElement = std::pair<std::reference_wrapper<Edge>, bool>; //a pair of the predecessor and the direction from predecessor to successor (true: forward, false: backward).
 using Path = std::vector<PathElement>; //a path in a FlowNetwork
 
 //find the augumenting path.
 std::pair<bool,Path> FindAugumentingPath(FlowNetwork &network, const T &source, const T &sink){
+    T zero = static_cast<T>(0);
     const T V = network.size();
     Edge dummyEdge{{"pred",source},{"suc", source},{"capasity",0},{"flow",0}};
     Path path(V,PathElement(dummyEdge,true));
@@ -32,7 +33,8 @@ std::pair<bool,Path> FindAugumentingPath(FlowNetwork &network, const T &source, 
         queue.pop();
         
         //search for the foward edges
-        for(auto &edge : network[pred].first){
+        for(auto &edgeRef : network[pred].first){
+            auto &edge = edgeRef.get();
             suc = edge["suc"];
             if(!isVisited[suc] && edge["flow"] < edge["capasity"]){
                 path[suc] = PathElement(edge, true);
@@ -46,9 +48,10 @@ std::pair<bool,Path> FindAugumentingPath(FlowNetwork &network, const T &source, 
         }
 
         //search for the backward edges
-        for(auto &edge : network[pred].second){
+        for(auto &edgeRef : network[pred].second){
+            auto &edge = edgeRef.get();
             suc = edge["pred"];
-            if(!isVisited[suc] && edge["flow"] > static_cast<T>(0)){
+            if(!isVisited[suc] && edge["flow"] > zero){
                 path[suc] = PathElement(edge, false);
                 isVisited[suc] = true;
                 queue.push(suc);
@@ -72,12 +75,13 @@ void EdmonsKarp(FlowNetwork &network, const T &source, const T &sink){
         suc = sink;
         dFlow = dFlowSup;
         while(suc != source){
+            auto &edge = path[suc].first.get();
             if(path[suc].second){
-                pred = path[suc].first.get()["pred"];
-                tmpFlow = path[suc].first.get()["capasity"] - path[suc].first.get()["flow"];
+                pred = edge["pred"];
+                tmpFlow = edge["capasity"] - edge["flow"];
             }else{
-                pred = path[suc].first.get()["suc"];
-                tmpFlow = path[suc].first.get()["flow"];
+                pred = edge["suc"];
+                tmpFlow = edge["flow"];
             }
             if(dFlow > tmpFlow){
                 dFlow = tmpFlow;
@@ -88,12 +92,13 @@ void EdmonsKarp(FlowNetwork &network, const T &source, const T &sink){
         //update the flow
         suc = sink;
         while(suc != source){
+            auto &edge = path[suc].first.get();
             if(path[suc].second){
-                pred = path[suc].first.get()["pred"];
-                path[suc].first.get()["flow"] += dFlow;
+                pred = edge["pred"];
+                edge["flow"] += dFlow;
             }else{
-                pred = path[suc].first.get()["suc"];
-                path[suc].first.get()["flow"] -= dFlow;
+                pred = edge["suc"];
+                edge["flow"] -= dFlow;
             }
             suc = pred;
         }
@@ -108,14 +113,17 @@ int main(int argc, char *argv[]){
     T u[M] = {0,1,3,0,2,4,1,2}, v[M] = {1,3,5,2,4,5,4,3};
     T c[M] = {3,2,3,2,3,2,2,2};
     FlowNetwork network(N);
+    Edge edge[M];
     for(T i=0; i<M; ++i){
-        network[u[i]].first.emplace_back(Edge{{"pred",u[i]},{"suc",v[i]},{"capasity",c[i]},{"flow",0}});
-        network[v[i]].second.emplace_back(Edge{{"pred",u[i]},{"suc",v[i]},{"capasity",c[i]},{"flow",0}});
+        edge[i] = Edge{{"pred",u[i]},{"suc",v[i]},{"capasity",c[i]},{"flow",0}};
+        network[u[i]].first.emplace_back(edge[i]);
+        network[v[i]].second.emplace_back(edge[i]);
     }
 
     EdmonsKarp(network, source, sink);
     for(auto edges : network){
-        for(auto edge : edges.first){
+        for(auto edgeRef : edges.first){
+            auto &edge = edgeRef.get();
             std::cout << edge["pred"] << " --> " << edge["suc"] << "\t:\t" << edge["flow"] << " / " << edge["capasity"] << std::endl;
         }
     }
