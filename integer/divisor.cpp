97efe620e0ace4f2fff5f3efd458c52fa26e8ea9
Yuichiro Dan <yuichirodan1022@gmail.com>
2019-10-05 16:21:11 +0900

    Add divisor.cpp: finds all divisors of given number.

---
+++ integer/divisor.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stack>
+#include <queue>
+#include <vector>
+
+template<class T>
+std::vector<T> getDivisors(T N){
+    std::queue<T> D_lower;
+    std::stack<T> D_upper;
+    for(T i=static_cast<T>(1); i*i<=N; ++i){
+        if(N%i == 0){
+            D_lower.emplace(i);
+            if(i*i != N){
+                D_upper.emplace(N/i);
+            }
+        }
+    }
+    std::vector<T> D;
+    while(!D_lower.empty()){
+        D.emplace_back(D_lower.front());
+        D_lower.pop();
+    }
+    while(!D_upper.empty()){
+        D.emplace_back(D_upper.top());
+        D_upper.pop();
+    }
+    return D;
+}
+
+int main(int argc, char *argv[]){
+    auto D = getDivisors(1024);
+    for(auto d : D){
+        std::cout << d << " ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
