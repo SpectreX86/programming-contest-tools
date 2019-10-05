c3a582d2a53fe703733c9c1fbabf771f4ee3e753
Yuichiro Dan <yuichirodan1022@gmail.com>
2019-10-05 16:18:01 +0900

    Update Dijkstra.cpp: Graph alias changed from map to vector.

--- graph/DijkstraSparse.cpp
+++ graph/DijkstraSparse.cpp
@@ -5,23 +5,28 @@
 #include <limits>
 #include <utility>
 #include <string>
+#include <numeric>
 
 //find the shortest path and its distance with Dijkstra algorithm.
 
+//if you would like this program to run faster, use pair or vector instead of map in Edge.
 template<class T>
-using Edge = std::map<std::string, T>; //for this algorithm, an edge must have "vertex" and "length" keys.
+using Edge = std::map<std::string, T>; //for this algorithm, an edge must have "successor" and "cost" keys.
 template<class T>
-using Graph = std::map<T, std::vector<Edge<T> > >;
+using Graph = std::vector<std::vector<Edge<T> > >;
 
 //The id of a vertex in the graph must be in [0,V).
 template<class T>
-std::pair<T, std::vector<T> > Dijkstra(Graph<T> &graph, const T &V, const T &start, const T &end){
+std::pair<std::vector<T>, std::vector<T> > Dijkstra(Graph<T> &graph, const T &start){
+    const T V = graph.size();
     const T distanceSup = std::numeric_limits<T>::max();
+    const T zero = static_cast<T>(0);
+
     std::vector<T> distance(V, distanceSup);
-    std::vector<T> predecessor(V, distanceSup);
+    std::vector<T> predecessor(V);
     std::vector<bool> isVisited(V, false);
-    distance[start] = 0;
-    predecessor[start] = start;
+    distance[start] = zero;
+    std::iota(predecessor.begin(),predecessor.end(),zero);
 
     auto compare = [](const std::pair<T,T> &a, const std::pair<T,T> &b){
         return a.second > b.second;
@@ -39,16 +44,16 @@ std::pair<T, std::vector<T> > Dijkstra(Graph<T> &graph, const T &V, const T &sta
         isVisited[pred] = true;
         queue.pop();
 
-        if(pred == end || distance[pred] == distanceSup){
+        if(distance[pred] == distanceSup){
             break;
         }
 
         for(auto &edge: graph[pred]){
-            suc = edge["vertex"];
+            suc = edge["successor"];
             if(isVisited[suc]){
                 continue;
             }
-            distTmp = distance[pred] + edge["length"];
+            distTmp = distance[pred] + edge["cost"];
             if(distance[suc] > distTmp){
                 distance[suc] = distTmp;
                 predecessor[suc] = pred;
@@ -57,7 +62,7 @@ std::pair<T, std::vector<T> > Dijkstra(Graph<T> &graph, const T &V, const T &sta
         }        
     }
 
-    return std::pair<T, std::vector<T> >(distance[end],predecessor);
+    return std::pair<std::vector<T>, std::vector<T> >(distance,predecessor);
 }
 
 int main(int argc, char *argv[]){
@@ -65,20 +70,22 @@ int main(int argc, char *argv[]){
     constexpr int M = 7;
     int u[M] = {0,1,2,4,3,2,0}, v[M] = {1,2,4,3,0,3,4};
     int w[M] = {2,3,1,7,8,5,4};
-    Graph<int> graph;
+    Graph<int> graph(N);
     for(int i=0; i<M; ++i){
-        graph[u[i]].emplace_back(Edge<int>{{"vertex",v[i]},{"length",w[i]}});
+        graph[u[i]].emplace_back(Edge<int>{{"successor",v[i]},{"cost",w[i]}});
     }
 
-    constexpr int S=0, T=3;
-    auto test = Dijkstra(graph, N, S, T);
-    std::cout << "distance: " << test.first << std::endl;
-    int p = T;
-    std::cout << "path: (goal) " << p;
-    while(p != test.second[p]){
-        p = test.second[p];
-        std::cout << " <-- " << p;
+    constexpr int S=0;
+    auto test = Dijkstra(graph, S);
+    for(int i=0; i<N; ++i){
+        int p = i;
+        std::cout << "distance: " << test.first[p] << "\t";
+        std::cout << "path: (goal) " << p;
+        while(p != test.second[p]){
+            p = test.second[p];
+            std::cout << " <-- " << p;
+        }
+        std::cout << " (start)" << std::endl;
     }
-    std::cout << " (start)" << std::endl;
     return 0;
 }
