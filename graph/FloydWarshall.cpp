88a8d68deb8658a6d11b5792b705e2bebe30abf3
Yuichiro Dan <yuichirodan1022@gmail.com>
2019-11-10 13:50:09 +0900

    Update FloydWarshall.cpp: codes rearranged.

--- graph/FloydWarshall.cpp
+++ graph/FloydWarshall.cpp
@@ -11,49 +11,50 @@
 template<class T>
 using Edge = std::map<std::string, T>; //for this algorithm, an edge must have "vertex" and "length" keys.
 template<class T>
-using Graph = std::map<T, std::vector<Edge<T> > >;
+using Graph = std::vector<std::vector<Edge<T> > >;
 
 //V is the number of the vertices of the graph. The id of a vertex in the graph must be in [0,V).
 template<class T>
-std::pair<std::vector< std::vector<T> >, std::vector< std::vector<T> > > FloydWarshall(Graph<T> &graph, const T &V){
-    const T distanceSup = std::numeric_limits<T>::max();
-    std::vector< std::vector<T> > distance(V, std::vector<T>(V,distanceSup));
-    std::vector< std::vector<T> > predecessor(V, std::vector<T>(V,distanceSup));
+std::pair<std::vector< std::vector<T> >, std::vector< std::vector<T> > > FloydWarshall(Graph<T> &graph){
+    const T V = graph.size();
+    const T sup = std::numeric_limits<T>::max();
+    const T zero = static_cast<T>(0);
+    std::vector< std::vector<T> > cost(V, std::vector<T>(V,sup));
+    std::vector< std::vector<T> > predecessor(V, std::vector<T>(V,sup));
     for(int i=0; i<V; ++i){
-        distance[i][i] = 0;
+        cost[i][i] = 0;
         predecessor[i][i] = i;
     }
     T pred, suc;
-    for(auto &edges : graph){
-        pred = edges.first;
-        for(auto &edge : edges.second){
+    for(pred=zero; pred<V; ++pred){
+        for(auto &edge : graph[pred]){
             suc = edge["vertex"];
-            distance[pred][suc] = edge["length"];
+            cost[pred][suc] = edge["length"];
             predecessor[pred][suc] = pred;
         }
     }
 
-    T distanceTmp;
+    T costTmp;
     T s,t;
-    for(T i=0; i<V; ++i){
-        for(s=0; s<V; ++s){
-            if(distance[s][i] == distanceSup){
+    for(T i=zero; i<V; ++i){
+        for(s=zero; s<V; ++s){
+            if(cost[s][i] == sup){
                 continue;
             }
-            for(t=0; t<V; ++t){
-                if(distance[i][t] == distanceSup){
+            for(t=zero; t<V; ++t){
+                if(cost[i][t] == sup){
                     continue;
                 }
-                distanceTmp = distance[s][i] + distance[i][t];
-                if(distance[s][t] > distanceTmp){
-                    distance[s][t] = distanceTmp;
+                costTmp = cost[s][i] + cost[i][t];
+                if(cost[s][t] > costTmp){
+                    cost[s][t] = costTmp;
                     predecessor[s][t] = predecessor[i][t];
                 }
             }
         }
     }
 
-    return std::pair<std::vector< std::vector<T> >, std::vector< std::vector<T> > >(distance,predecessor);
+    return std::make_pair(cost, predecessor);
 }
 
 int main(int argc, char *argv[]){
@@ -61,12 +62,12 @@ int main(int argc, char *argv[]){
     constexpr int M = 7;
     int u[M] = {0,1,2,3,0,2,4}, v[M] = {1,2,3,0,4,4,3};
     int w[M] = {2,3,5,8,4,1,7};
-    Graph<int> graph;
+    Graph<int> graph(N);
     for(int i=0; i<M; ++i){
         graph[u[i]].emplace_back(Edge<int>{{"vertex",v[i]},{"length",w[i]}});
     }
 
-    auto test = FloydWarshall(graph, N);
+    auto test = FloydWarshall(graph);
 
     int i;
     for(int s=0; s<N; ++s){
